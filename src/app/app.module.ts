f0c877575a9a0e74d266c2e988b90ab2f21e24d1
Thiago Arthur <thlimabsgim@gmail.com>
2023-12-01 00:35:04 -0400

    dropdown now working properly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HeaderComponent} from "../core/header/header.component";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AngularMaterialModule} from "../shared/angular-material/angular-material.module";
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
