09f19aebb93838dcd8c26e17249c6352586a1c3e
Thiago Arthur <thlimabsgim@gmail.com>
2023-12-06 02:41:32 -0400

    Starting the movie details with a modal to embed the trailer

--- src/app/feature/movie-detail/movie-detail.component.ts
+++ src/app/feature/movie-detail/movie-detail.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {StorageService} from "../../service/storage.service";
 import {Movie} from "../../interface/movie";
+import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-movie-detail',
@@ -9,18 +10,32 @@ import {Movie} from "../../interface/movie";
 })
 export class MovieDetailComponent implements OnInit, OnDestroy{
   movieDetail? : Movie | null
+  videoCode: string | null = null;
+  embedUrl: string | null = null;
 
   constructor(
-    private storageService: StorageService
+    private storageService: StorageService,
+    private sanitizer: DomSanitizer
   ) {}
 
   ngOnInit(): void {
     this.movieDetail = this.storageService.getMovieDetail()
+    this.extractVideoCode()
+  }
+
+  private extractVideoCode(): void {
+    const videoUrl = this.movieDetail?.trailer
+    const match = videoUrl?.match(/(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/);
+
+    this.videoCode = match ? match[1] : null;
+    this.embedUrl = this.videoCode ? `https://www.youtube.com/embed/${this.videoCode}` : null;
+    console.log(this.embedUrl)
   }
 
   ngOnDestroy() {
     this.storageService.clearMovieDetail()
   }
+
 
 
 }
